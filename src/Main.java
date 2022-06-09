abd8f65d14c7bec81c36498b4350f5460a6afba0
Rohin <kotianra@mail.uc.edu>
2022-06-09 11:20:47 -0400

    Share Init

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ Main
 {
     public static void main(String[] args)
     {
-        double purchasePrice = 2.00;
+        double purchasePrice = 100.00;
         double TAX_RATE = 0.05;
         double salesTax = purchasePrice * TAX_RATE;
         System.out.println("The Sales Tax is " + salesTax);

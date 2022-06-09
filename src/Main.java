4284b0424b22673adea4ddda580bc2d3c7b1cc13
Rohin <kotianra@mail.uc.edu>
2022-06-09 11:19:35 -0400

    Share Init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class
+Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 2.00;
+        double TAX_RATE = 0.05;
+        double salesTax = purchasePrice * TAX_RATE;
+        System.out.println("The Sales Tax is " + salesTax);
+    }
+}
